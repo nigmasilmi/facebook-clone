f45ec9d916877f90c71c319256acb597fc95243d
nigmasilmi <nigmasilmi.web@gmail.com>
2020-11-22 12:44:44 -0300

    sidebar component added and styled

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import Header from './components/Header/Header';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      {/* App Body */}
-      {/* Sidebar */}
-      {/* Feed */}
-      {/* Widgets */}
+    <div className="app">
+      <div className="app__body">
+        <Header />
+        <Sidebar />
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+      </div>
+
     </div>
   );
 }

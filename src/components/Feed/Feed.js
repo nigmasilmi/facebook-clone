f73d64e50c7048df6539eba9dfc37c2d8a3c9999
nigmasilmi <nigmasilmi.web@gmail.com>
2020-11-22 19:23:39 -0300

    post component built and styled

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -2,14 +2,27 @@ import React from 'react';
 import './Feed.css';
 import StoryReel from './StoryReel/StoryReel';
 import MessageSender from '../MessageSender/MessageSender';
+import Post from '../Post/Post';
 
 const Feed = (props) => {
     return (
         <div className="feed">
             <StoryReel />
-            {/* History reel */}
-            {/* Message sender */}
             <MessageSender />
+            <Post
+                profilePic='https://res.cloudinary.com/dz3gm9c3w/image/upload/v1606064845/facebook-clone/freepik-avatar-07_urjnav.jpg'
+                username='Betty'
+                message='This works'
+                timestamp='this is a timestamp'
+                image='https://i2.wp.com/dianaurban.com/wp-content/uploads/2017/07/01-cat-stretching-feet.gif?resize=500%2C399&ssl=1'
+            />
+            <Post
+                profilePic='https://res.cloudinary.com/dz3gm9c3w/image/upload/v1606064845/facebook-clone/freepik-avatar-04_raqvra.jpg'
+                username='Anita'
+                message='This works'
+                timestamp='this is a timestamp'
+                image=''
+            />
         </div>
     )
 }
---
+++ src/components/Post/Post.js
@@ -0,0 +1,52 @@
+import { Avatar } from '@material-ui/core';
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import ChatBubbleIcon from '@material-ui/icons/ChatBubble';
+import NearMeIcon from '@material-ui/icons/NearMe';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import React from 'react';
+import './Post.css';
+
+const Post = ({ profilePic, image, username, timestamp, message }) => {
+    return (
+        <div className="post">
+            <div className="post__top">
+                <Avatar
+                    src={profilePic}
+                    className="post__avatar"
+                />
+                <div className="post__topInfo">
+                    <h3>{username}</h3>
+                    <p>Timestamp</p>
+                </div>
+
+            </div>
+            <div className="post__bottom">
+                <p>{message}</p>
+            </div>
+            <div className="post__img">
+                <img src={image} alt='' />
+            </div>
+            <div className="post__options">
+                <div className="post__option">
+                    <ThumbUpIcon />
+                    <p>Like</p>
+                </div>
+                <div className="post__option">
+                    <ChatBubbleIcon />
+                    <p>Comment</p>
+                </div>
+                <div className="post__option">
+                    <NearMeIcon />
+                    <p>Share</p>
+                </div>
+                <div className="post__option">
+                    <AccountCircleIcon />
+                    <ExpandMoreIcon />
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Post;
